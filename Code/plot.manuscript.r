8d670f9a485094735570f6f9ae31005fcb699ac7
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2024-12-04 18:31:33 +1200

    update plot.manuscript.r

--- Code/plot.manuscript.r
+++ Code/plot.manuscript.r
@@ -36,6 +36,35 @@
 		names(par.list) = names(rep.list) = model_name
 
 #_____________________________________________________________________________________________________________________________
-# extract the relative recruitment (overall rec dev)
-	diag_relrec = as.data.table(rel_rec(par.list[[1]]))
+# extract quantities
+    # the relative recruitment (overall rec dev)
+	diag_relrec = as.data.table(rel_rec(par.list[[1]])) %>%
+                  .[,ts:=as.numeric(year)+(as.numeric(season)-1)/4] %>%
+                  .[,type:="relrec"] %>%
+                  .[,.(type,ts,value)]
+    # all catch in mt, exclude LL fleets (3,6,9,17,21,23,27,31)
+    diag_catch = as.data.table(catch_pred(rep.list[[1]])) %>%
+                 .[!(unit%in%c("3","6","9","17","21","23","27","31"))] %>%
+                 .[,.(value=sum(value)),by=.(year,season)] %>%
+                 .[,ts:=as.numeric(year)+(as.numeric(season)-1)/4] %>%
+                  .[,type:="catch"] %>%
+                  .[,.(type,ts,value)]
+    # obs cpue
+    	frq = readfrq(paste0(proj_dir,"/Model_Runs/Length100_GrowthDiag_Mix1_H0.8/skj.frq"))
+		diag_obscpue = as.data.table(cateffpen(frq)) %>%
+		 			   .[,id:=paste0(year,"_",month,"_",fishery)] %>%
+					   .[,.(id,year,month,week,fishery,catch,effort,penalty)] %>%
+					   .[,ts:=as.numeric(year)+(as.numeric(month)-2)/12] %>%
+					   .[,cpue:=catch/effort] %>%
+                       .[fishery %in% c(1,4,7,22,24,28,12,19)] %>%
+                       .[penalty>0] %>%
+                       .[,sd:=1/sqrt(2*penalty)] %>%
+                       .[,mean_penalty:=mean(penalty),by=fishery] %>%
+                       .[,mean_sd:=mean(sd),by=fishery]
+               
+    # ggplot(diag_obscpue) +
+    # facet_wrap(~fleet) +
+    # geom_path(aes(x=ts,y=value))
+
+
 	
